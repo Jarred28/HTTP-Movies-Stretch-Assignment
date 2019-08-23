9d79751122771230e8118d5bd50996759e0fdbc1
jarred richards <jarred2828@gmail.com>
2019-08-23 11:10:11 -0400

    MVP

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,7 +5,7 @@ export default class SavedList extends Component {
     super(props);
   }
 
-  render() {
+  render() { 
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
